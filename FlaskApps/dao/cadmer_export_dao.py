164c1526f7ae2a6588142c3da3abe7ebd59e74d8
qBitLocker <samuguerrilha@gmail.com>
2025-01-27 16:49:44 -0200

    Criando uma estilização simples para o formulário

--- FlaskApps/dao/cadmer_export_dao.py
+++ FlaskApps/dao/cadmer_export_dao.py
@@ -8,29 +8,39 @@ class ProductDataAccess():
         # Não se esqueça de adicionar a chave 'mysql' no dicionário app.extensions, tudo dentro de app.py
         self.mysql = current_app.extensions['mysql']
     
-    # Considerando o caso mais simples
-    def get_product_by_id (self, product_id:str):
-        print (product_id)
-
-        print ('Creating Cursor...')
-        cursor = self.mysql.connection.cursor()
-
-        print ('Defininf the query...')
-        # Define the query to fetch product details
-        query = '''
-            SELECT descricao, app_precovenda as venda
-            FROM cadmer_exportacao
-            WHERE codbarra = %s
-        '''
-
+    # Private method
+    def __check_product_id__(self, product_id: str) -> bool:
+        # Tentativa 1 - Tentar converte para um num inteiro
         try:
-            cursor.execute(query, (product_id,))  # Execute the query with the provided product_id
-            result = cursor.fetchone()  # Fetch one result
-        except MySQLdb.Error as e:
-            print(f"Error fetching product by id: {e}")
-            result = None
-        finally:
-            cursor.close()
+            id = int (product_id)
+        except:
+            print ("\'"+product_id + "\' não passou no validação de entrada!")
+            return False
+        return True
+    
+    # Considerando o caso mais simples
+    def get_product_by_id(self, product_id:str):
+        # Verificar se product_id é um parâmetro válido
+        if self.__check_product_id__(product_id):
+            cursor = self.mysql.connection.cursor()
 
-        # Return the result, or None if no product was found
-        return result
+            # Define the query to fetch product details
+            query = '''
+                SELECT descricao, app_precovenda as venda
+                FROM cadmer_exportacao
+                WHERE codbarra = %s
+            '''
+
+            try:
+                cursor.execute(query, (product_id,))  # Execute the query with the provided product_id
+                result = cursor.fetchone()  # Fetch one result
+            except MySQLdb.Error as e:
+                print(f"Error fetching product by id: {e}")
+                result = None
+            finally:
+                cursor.close()
+
+            # Return the result, or None if no product was found
+            return result
+        else:
+            return None
